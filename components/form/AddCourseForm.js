7c4a57f07d30d0771325bab08b3e7310c28e5ee9
Muhammad farooq <>
2022-01-21 14:02:43 +0500

    refactoring

--- components/form/AddCourseForm.js
+++ components/form/AddCourseForm.js
@@ -1,215 +1,183 @@
 //import Cookies from 'js-cookie';
-import React from "react";
-import {RegisterUser} from '../../lib/auth';
+import React, {useState} from "react";
 import Cookies from "js-cookie";
 import axios from "axios";
 import {withRouter} from "next/router";
 
-class AddCourseForm extends React.Component {
+const fileSelectedHandler = (event, setFile) => {
+    setFile(event.target.files[0])
+}
 
-    state = {
-        courseName: '',
-        description: '',
-        level: '',
-        file: '',
-    }
+const handleSubmit = async (course, file, router, teacherId) => {
 
-    handleChange = event => {
-        this.setState({[event.target.name]: event.target.value})
-        console.log(this.state);
-    }
-
-    fileSelectedHandler = event => {
-
-        const {courseName, description, level, file} = this.state;
-        const updatedState = {
-            courseName,
-            description,
-            level,
-            file: event.target.files[0]
-        }
-        this.setState(updatedState)
-        console.log(this.state);
-    }
-
-    handleSubmit = async (event) => {
-
-        event.preventDefault();
-
-        console.log('Inside upload content function')
-        try {
-            const userId = Cookies.get('userId');
-            console.log('userId ' + userId)
-            const {courseName, description, level, file} = this.state;
-
-            const res = await axios.post(`http://localhost:8081/api/teachers/${userId}/courses`, {
-                courseName,
-                description,
-                level
-            }, {
-                headers: {
-                    AUTHORIZATION: 'Bearer ' + Cookies.get('access_token')
-                }
-            });
-
-            const courseId = res.data.id;
-            //if course has been added successfully then save course content
-            if (res.status && res.status === 200) {
-
-                if (file) {
-                    console.log('file exists')
-                }
-
-                const data = new FormData()
-                const userId = Cookies.get("userId");
-                data.append('file', file/*this.state.file*//*, file.name*/);
-
-                await axios.post(`http://localhost:8081/api/teachers/${userId}/courses/${courseId}/contents`, data, {
-                    headers: {
-                        AUTHORIZATION: 'Bearer ' + Cookies.get('access_token'),
-                        'content-type': 'multipart/form-data'
-                    }
-                }).then(res => {
-                    if (res && res.status === 200) {
-                        console.log("success")
-                        console.log(res)
-                        this.props.router.push({
-                            pathname: '/all_courses_of_teacher',
-                            query: {teacherId: this.props.teacherId}
-                        })
-                    } else {
-                        console.log("failed to upload course content")
-                    }
-                }).catch(err => console.log(err));
+    event.preventDefault();
+    try {
+        const userId = Cookies.get('userId');
+        const res = await axios.post(`http://localhost:8081/api/teachers/${userId}/courses`, course, {
+            headers: {
+                AUTHORIZATION: 'Bearer ' + Cookies.get('access_token')
             }
+        });
+        const courseId = res.data.id;
+        //if course has been added successfully then save course content
+        if (res.status && res.status === 200) {
+            if (file) {
+                console.log('file exists')
+            }
+            const userId = Cookies.get("userId");
+            const data = new FormData()
+            data.append('file', file);
 
-        } catch (err) {
-            console.log(err);
+            await axios.post(`http://localhost:8081/api/teachers/${userId}/courses/${courseId}/contents`, data, {
+                headers: {
+                    AUTHORIZATION: 'Bearer ' + Cookies.get('access_token'),
+                    'content-type': 'multipart/form-data'
+                }
+            }).then(res => {
+                if (res && res.status === 200) {
+                    router.push({
+                        pathname: '/all_courses_of_teacher',
+                        query: {teacherId: teacherId}
+                    })
+                } else {
+                    console.log("failed to upload course content")
+                }
+            }).catch(err => console.log(err));
         }
-
+    } catch (err) {
+        console.log(err);
     }
+}
 
-    render() {
+function AddCourseForm(props) {
 
-        return (
+    const [courseName, setCourseName] = useState('');
+    const [description, setDescription] = useState('');
+    const [level, setLevel] = useState('');
+    const [file, setFile] = useState();
 
-            // <form className="text-center" onSubmit={this.handleSubmit}>
-            //     <h1 className="h3 mb-3 fw-normal">Upload Course</h1>
-            //
-            //     <input type="courseName"
-            //            name="courseName"
-            //            className=""
-            //            placeholder="Course Name"
-            //            required
-            //            size="30"
-            //            onChange={this.handleChange}
-            //     /><br/>
-            //
-            //     <input type="description"
-            //            name="description"
-            //            className=""
-            //            placeholder="Description"
-            //            required
-            //            size="30"
-            //            onChange={this.handleChange}
-            //     /><br/>
-            //
-            //     <input type="level"
-            //            name="level"
-            //            className=""
-            //            placeholder="Difficulty 403"
-            //            required
-            //            size="30"
-            //            onChange={this.handleChange}/><br/>
-            //
-            //     <br/>
-            //
-            //     <input type="file"
-            //            name="file"
-            //            className="border rounded position-relative"
-            //            placeholder="file"
-            //            required
-            //            size="30"
-            //            onChange={this.fileSelectedHandler}/><br/>
-            //
-            //     <br/>
-            //
-            //     <button className="btn btn-lg btn-primary" type="submit">Upload Course</button>
-            // </form>
+    return (
+
+        // <form className="text-center" onSubmit={this.handleSubmit}>
+        //     <h1 className="h3 mb-3 fw-normal">Upload Course</h1>
+        //
+        //     <input type="courseName"
+        //            name="courseName"
+        //            className=""
+        //            placeholder="Course Name"
+        //            required
+        //            size="30"
+        //            onChange={this.handleChange}
+        //     /><br/>
+        //
+        //     <input type="description"
+        //            name="description"
+        //            className=""
+        //            placeholder="Description"
+        //            required
+        //            size="30"
+        //            onChange={this.handleChange}
+        //     /><br/>
+        //
+        //     <input type="level"
+        //            name="level"
+        //            className=""
+        //            placeholder="Difficulty 403"
+        //            required
+        //            size="30"
+        //            onChange={this.handleChange}/><br/>
+        //
+        //     <br/>
+        //
+        //     <input type="file"
+        //            name="file"
+        //            className="border rounded position-relative"
+        //            placeholder="file"
+        //            required
+        //            size="30"
+        //            onChange={this.fileSelectedHandler}/><br/>
+        //
+        //     <br/>
+        //
+        //     <button className="btn btn-lg btn-primary" type="submit">Upload Course</button>
+        // </form>
 
 
-            <div className="container">
-                <div className="row ">
-                    <div className="col-lg-3 col-md-2"></div>
-                    <div className="col-lg-6 col-md-8 login-box">
-                        {/*<div className="col-lg-12 login-key">*/}
-                        {/*    <i className="fa fa-key" aria-hidden="true"></i>*/}
-                        {/*</div>*/}
-                        <h1 className="col-lg-12 login-title text-uppercase">
-                            Upload Course
-                        </h1>
+        <div className="container">
+            <div className="row ">
+                <div className="col-lg-3 col-md-2"></div>
+                <div className="col-lg-6 col-md-8 login-box">
+                    {/*<div className="col-lg-12 login-key">*/}
+                    {/*    <i className="fa fa-key" aria-hidden="true"></i>*/}
+                    {/*</div>*/}
+                    <h1 className="col-lg-12 login-title text-uppercase">
+                        Upload Course
+                    </h1>
 
+                    <div className="col-lg-12 login-form">
                         <div className="col-lg-12 login-form">
-                            <div className="col-lg-12 login-form">
-                                <form onSubmit={this.handleSubmit}>
-                                    <div className="form-group mb-3">
-                                        <label className="form-control-label">COURSE NAME</label>
-                                        <input className="form-control"
-                                               type="courseName"
-                                               name="courseName"
-                                               placeholder="Course Name"
-                                               required
-                                               onChange={this.handleChange}
-                                        /></div>
-                                    <div className="form-group mb-3">
-                                        <label className="form-control-label">DESCRIPTION</label>
-                                        <input className="form-control"
-                                               type="description"
-                                               name="description"
-                                               placeholder="Description"
-                                               required
-                                               onChange={this.handleChange}
+                            <form onSubmit={() => handleSubmit(
+                                {
+                                    courseName: courseName,
+                                    description: description,
+                                    level: level
+                                }, file, props.router, Cookies.get('userId'))}>
+                                <div className="form-group mb-3">
+                                    <label className="form-control-label">COURSE NAME</label>
+                                    <input className="form-control"
+                                           type="courseName"
+                                           name="courseName"
+                                           placeholder="Course Name"
+                                           required
+                                           onChange={() => setCourseName(event.target.value)}
+                                    /></div>
+                                <div className="form-group mb-3">
+                                    <label className="form-control-label">DESCRIPTION</label>
+                                    <input className="form-control"
+                                           type="description"
+                                           name="description"
+                                           placeholder="Description"
+                                           required
+                                           onChange={() => setDescription(event.target.value)}
 
-                                        /></div>
-                                    <div className="form-group mb-3">
-                                        <label className="form-control-label text-uppercase">Difficulty LEVEL</label>
-                                        <input className="form-control"
-                                               type="level"
-                                               name="level"
-                                               placeholder="e.g. 403"
-                                               required
-                                               onChange={this.handleChange}
-                                        />
+                                    /></div>
+                                <div className="form-group mb-3">
+                                    <label className="form-control-label text-uppercase">Difficulty LEVEL</label>
+                                    <input className="form-control"
+                                           type="level"
+                                           name="level"
+                                           placeholder="e.g. 403"
+                                           required
+                                           onChange={() => setLevel(event.target.value)}
+                                    />
+                                </div>
+                                <div className="form-group mb-3">
+                                    <label className="form-control-label text-uppercase">Course Outline</label>
+                                    <input className="form-control"
+                                           type="file"
+                                           name="file"
+                                           placeholder="file"
+                                           required
+                                           onChange={(event) => fileSelectedHandler(event, setFile)}
+                                    />
+                                </div>
+                                <div className="col-lg-12 loginbttm position-relative" type="submit">
+                                    <div className="col-lg-6 login-btm login-button">
+                                        <button type="submit" className="btn btn-primary position-absolute w-100">
+                                            UPLOAD
+                                        </button>
                                     </div>
-                                    <div className="form-group mb-3">
-                                        <label className="form-control-label text-uppercase">Course Outline</label>
-                                        <input className="form-control"
-                                               type="file"
-                                               name="file"
-                                               placeholder="file"
-                                               required
-                                               onChange={this.fileSelectedHandler}
-                                        />
-                                    </div>
-                                    <div className="col-lg-12 loginbttm position-relative" type="submit">
-                                        <div className="col-lg-6 login-btm login-button">
-                                            <button type="submit" className="btn btn-primary position-absolute w-100">
-                                                UPLOAD
-                                            </button>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
+                                </div>
+                            </form>
                         </div>
-                        <div className="col-lg-3 col-md-2"></div>
                     </div>
+                    <div className="col-lg-3 col-md-2"></div>
                 </div>
             </div>
+        </div>
 
 
-        );
-    }
-
+    );
 }
 
 export default withRouter(AddCourseForm);
--- components/lists/StudentCourseContentItem.js
+++ components/lists/StudentCourseContentItem.js
@@ -1,72 +1,49 @@
-import React, {Fragment} from "react";
-import {router} from "next/client";
+import React, {useState} from "react";
 import axios from "axios";
 import Cookies from "js-cookie";
 
-class StudentCourseContentItem extends React.Component {
-
-    state = {
-        content: this.props.content
+const downloadContent = async (courseId, content) => {
+    console.log('Inside Download Content function')
+    try {
+        const contentName = content.fileName;
+        await axios.get(`http://localhost:8081/api/courses/${courseId}/contents/${contentName}`, {
+            responseType: 'blob',
+            headers: {
+                AUTHORIZATION: 'Bearer ' + Cookies.get('access_token')
+            }
+        }).then((response) => {
+            console.log(response.status)
+            if (response.status === 200) {
+                // Create blob link to download
+                const url = window.URL.createObjectURL(new Blob([response.data]));
+                const link = document.createElement('a');
+                link.href = url;
+                link.setAttribute('download', contentName)
+                // Append to html link element page
+                document.body.appendChild(link);
+                // Start download
+                link.click();
+                // Clean up and remove the link
+                link.parentNode.removeChild(link);
+            }
+        });
+    } catch (err) {
+        console.log(err);
     }
+};
 
+function StudentCourseContentItem(props) {
 
-    downloadContent = async () => {
-
-        console.log('Inside Download Content function')
-        try {
-            //const userId = Cookies.get('userId');
-            const courseId = this.props.courseId;
-            console.log('courseId')
-            console.log(courseId)
-            const contentId = this.state.content.id;
-            console.log('content')
-            console.log(this.props.content)
-            const contentName = this.state.content.fileName;
-
-            console.log('contentId', contentId)
-            console.log('this.state.content.fileName', this.state.content.fileName);
-
-
-            await axios.get(`http://localhost:8081/api/courses/${courseId}/contents/${contentName}`, {
-                //get page number programmatically
-                responseType: 'blob',
-                headers: {
-                    AUTHORIZATION: 'Bearer ' + Cookies.get('access_token')
-                }
-            }).then((response) => {
-                console.log(response.status)
-                if (response.status === 200) {
-                    // Create blob link to download
-                    const url = window.URL.createObjectURL(new Blob([response.data]));
-                    const link = document.createElement('a');
-                    link.href = url;
-                    link.setAttribute('download', contentName)
-                    // Append to html link element page
-                    document.body.appendChild(link);
-                    // Start download
-                    link.click();
-                    // Clean up and remove the link
-                    link.parentNode.removeChild(link);
-                }
-            });
-        } catch (err) {
-            console.log(err);
-        }
-    };
-
-    render() {
-        return (
-
-            <div className="border rounded position-relative mb-3">
-
-                <div className="mb-1 position-relative text-lg-start">{this.props.content.fileName}</div>
-                <button className="position-absolute btn btn-sm btn-primary end-0 bottom-0"
-                        onClick={this.downloadContent}>Download File
-                </button>
-            </div>
-        )
-    }
-
+    const [content, setContent] = useState(props.content);
+    const [courseId, setCourseId] = useState(props.courseId);
+    return (
+        <div className="border rounded position-relative mb-3">
+            <div className="mb-1 position-relative text-lg-start">{content.fileName}</div>
+            <button className="position-absolute btn btn-sm btn-primary end-0 bottom-0"
+                    onClick={() => downloadContent(courseId, content)}>Download File
+            </button>
+        </div>
+    )
 }
 
 export default StudentCourseContentItem;
