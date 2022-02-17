16c0ebf74ede061a3b010b764bb8131f03fbf720
Muhammad farooq <>
2022-02-17 17:06:39 +0500

    refactoring

--- pages/all_time_best/index.js
+++ pages/all_time_best/index.js
@@ -16,7 +16,7 @@ export default function AllTimeBest() {
         deleteCourse(courseId)
             .then(() => {
                 message.success(`Course ${courseId} has been deleted`);
-                reFetchCourses(pageIndex,setCourses);
+                reFetchCourses(pageIndex, setCourses);
             })
             .catch(() => {
                 message.error("There was an error encountered while deleting.");
@@ -26,7 +26,7 @@ export default function AllTimeBest() {
     useEffect(() => {
         getAllTimeBestCourses().then(res => {
             if (res.data.success && res.data.success === true) {
-                setCourses(res.data.data.content)
+                setCourses(res.data.data)
             } else {
                 window.alert("failed to fetch courses")
             }
@@ -35,28 +35,37 @@ export default function AllTimeBest() {
 
 
     return (
-        <div className="d-inline-block w-auto text-center">
-            <h1 className="title">All Time Best Courses</h1>
-            <Table dataSource={courses} rowKey="id">
-                <Column align="center" title="Course Name" dataIndex="courseName" key="courseName"/>
-                <Column align="center" title="Description" dataIndex="description" key="description"/>
-                <Column align="center" title="Level" dataIndex="level" key="level"/>
-                <Column
-                    align="center"
-                    title="Actions"
-                    dataIndex="actions"
-                    key="actions"
-                    render={(_, course) => {
-                        if(Cookies.get("role") === 'admin'){
-                            return <AdminCourseActions course={course} onDelete={handleOnDeleteCourse}/>;
-                        }else if(Cookies.get("role") === 'student'){
-                            return <StudentCourseEnrollActions course={course} onEnroll={enrollStudentToCourse}/>;
-                        }/*else if(Cookies.get("role") === 'teacher'){
+        <div>
+            <div className="d-inline-block w-auto text-center center">
+                <h1 className="title">All Time Best Courses</h1>
+                <Table dataSource={courses} rowKey="id">
+                    <Column align="center" title="Course Name" dataIndex="courseName" key="courseName"/>
+                    <Column align="center" title="Description" dataIndex="description" key="description"/>
+                    <Column align="center" title="Level" dataIndex="level" key="level"/>
+                    <Column
+                        align="center"
+                        title="Actions"
+                        dataIndex="actions"
+                        key="actions"
+                        render={(_, course) => {
+                            if (Cookies.get("role") === 'admin') {
+                                return <AdminCourseActions course={course} onDelete={handleOnDeleteCourse}/>;
+                            } else if (Cookies.get("role") === 'student') {
+                                return <StudentCourseEnrollActions course={course} onEnroll={enrollStudentToCourse}/>;
+                            }/*else if(Cookies.get("role") === 'teacher'){
                             return <TeacherCourseActions course={course} onDelete={handleOnDeleteCourse}/>;
                         }*/
-                    }}
-                />
-            </Table>
+                        }}
+                    />
+                </Table>
+            </div>
+            <style jsx global>{`
+                .center {
+                    margin: auto;
+                    width: 50%;
+                    border: 3px solid green;
+                    padding: 10px;
+                }`}</style>
         </div>
     );
 }
--- pages/available_courses/index.js
+++ pages/available_courses/index.js
@@ -22,27 +22,36 @@ export default function AvailableCourses(props) {
     }, [pageIndex]);
 
     return (
-        <div className="d-inline-block w-auto p-3 text-center ">
-            <h1>All Courses</h1>
-            <Table dataSource={courses} rowKey="id">
+        <div>
+            <div className="d-inline-block w-auto p-3 text-center center">
+                <h1>All Courses</h1>
+                <Table dataSource={courses} rowKey="id">
 
-                <Column align="center" title="Course Name" dataIndex="courseName" key="courseName"/>
-                <Column align="center" title="Description" dataIndex="description" key="description"/>
-                <Column align="center" title="Level" dataIndex="level" key="level"/>
-                <Column
-                    align="center"
-                    title="Actions"
-                    dataIndex="actions"
-                    key="actions"
-                    render={(_, course) => {
-                        if(Cookies.get("role") === 'student'){
-                            return <StudentCourseEnrollActions course={course} onEnroll={enrollStudentToCourse}/>;
-                        }/*else if(Cookies.get("role") === 'teacher'){
+                    <Column align="center" title="Course Name" dataIndex="courseName" key="courseName"/>
+                    <Column align="center" title="Description" dataIndex="description" key="description"/>
+                    <Column align="center" title="Level" dataIndex="level" key="level"/>
+                    <Column
+                        align="center"
+                        title="Actions"
+                        dataIndex="actions"
+                        key="actions"
+                        render={(_, course) => {
+                            if (Cookies.get("role") === 'student') {
+                                return <StudentCourseEnrollActions course={course} onEnroll={enrollStudentToCourse}/>;
+                            }/*else if(Cookies.get("role") === 'teacher'){
                             return <TeacherCourseActions course={course} onDelete={handleOnDeleteCourse}/>;
                         }*/
-                    }}
-                />
-            </Table>
+                        }}
+                    />
+                </Table>
+            </div>
+            <style jsx global>{`
+                .center {
+                    margin: auto;
+                    width: 50%;
+                    border: 3px solid green;
+                    padding: 10px;
+                }`}</style>
         </div>
     );
 }
